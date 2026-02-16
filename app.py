244bbd37e11bba516bb91880ec2330673fb683ed
Art444666 <goritskijartem0@gmail.com>
2026-02-16 15:32:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -382,27 +382,6 @@ HTML = """
 
      <button onclick="location.href='/logout'" style="margin-top:20px; color:#ff4b4b; background:none; border:none; cursor:pointer; width:100%; text-align:left; font-weight:bold; padding:0;">Выйти из аккаунта</button>
 
-    <svg
-      class="done-svg"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path
-        class="done done-cloud"
-        pathLength="100"
-        d="M 6.5,20 Q 4.22,20 2.61,18.43 1,16.85 1,14.58 1,12.63 2.17,11.1 3.35,9.57 5.25,9.15 5.88,6.85 7.75,5.43 9.63,4 12,4 14.93,4 16.96,6.04 19,8.07 19,11 q 1.73,0.2 2.86,1.5 1.14,1.28 1.14,3 0,1.88 -1.31,3.19 Q 20.38,20 18.5,20 Z"
-      ></path>
-      <path
-        class="done done-check"
-        pathLength="100"
-        d="M 7.515,12.74 10.34143,15.563569 15.275,10.625"
-      ></path>
-    </svg>
-  </svg>
-  <div class="txt-upload">Скачать на Пк</div>
-</button>
-</a>
-
 </div>
 
 
@@ -1015,6 +994,7 @@ def show_users():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
 
